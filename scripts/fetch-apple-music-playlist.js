f448aed905bf368bb9102af99ac5744b20cba799
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2023-05-09 18:12:59 -0400

    Refactor fetch methods

--- scripts/fetch-apple-music-playlist.js
+++ scripts/fetch-apple-music-playlist.js
@@ -1,29 +1,40 @@
 const puppeteer = require("puppeteer");
 
-const urlFavorites2023 = `https://embed.music.apple.com/us/playlist/2023-absolute-favorites/pl.u-9N9L24eFADqk5`;
+/**
+ * @typedef {song: string, artist: string} Song
+ */
 
-(async () => {
-  /**
-   * Change a newline-delimited playlist item into an artist-song object.
-   * @param {string} playlistItem
-   * @return { {song: string, artist: string} | null}
-   */
-  const createSongDataFromPlaylistItem = (playlistItem) => {
-    if (!playlistItem) {
-      return null;
-    }
+/**
+ * Change a newline-delimited playlist item into an artist-song object.
+ * @param {string} playlistItem
+ * @return { Song | null}
+ */
+function createSongDataFromPlaylistItem(playlistItem) {
+  if (!playlistItem) {
+    return null;
+  }
 
-    const [song, artist] = playlistItem.split("\n");
-    return {
-      artist,
-      song,
-    };
+  const [song, artist] = playlistItem.split("\n");
+  return {
+    artist,
+    song,
   };
+}
+
+/**
+ * Returns an array of Song object after fetching and parsing an Apple Music embed playlist.
+ * @param {string} url - A URL to an Apple Music embed playlist
+ * @return {Promise<Awaited<Song>[]|null>}
+ */
+async function getSongsFromEmbedUrl(url) {
+  if (!url) {
+    return null;
+  }
 
   const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
 
-  await page.goto(urlFavorites2023);
+  await page.goto(url);
 
   const element = await page.waitForSelector(`main >>> embed-audio-tracklist`);
   const itemElements = await element.$$(
@@ -40,4 +51,15 @@ const urlFavorites2023 = `https://embed.music.apple.com/us/playlist/2023-absolut
   await browser?.close();
 
   return songs;
+}
+
+(async () => {
+  const urlFavorites2023 = `https://embed.music.apple.com/us/playlist/2023-absolute-favorites/pl.u-9N9L24eFADqk5`;
+  const urlGoodListening2023 = `https://embed.music.apple.com/us/playlist/2023-good-listenin/pl.u-DdAN8EosKAZ8R`;
+
+  const favoriteSongs = await getSongsFromEmbedUrl(urlFavorites2023);
+  console.log(favoriteSongs);
+
+  const goodSongs = await getSongsFromEmbedUrl(urlGoodListening2023);
+  console.log(goodSongs);
 })();
